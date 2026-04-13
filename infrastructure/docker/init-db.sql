f3fb73162e0db8ca37a53037bf59d0c7179d477f
Hernan Jurado <hernan.jurado@udla.edu.ec>
2026-04-13 15:37:45 -0500

    infra: add docker-compose with PostgreSQL and RabbitMQ

---
+++ infrastructure/docker/init-db.sql
@@ -0,0 +1,9 @@
+-- Crear bases de datos para cada microservicio
+CREATE DATABASE db_auth;
+CREATE DATABASE db_estudiantes;
+CREATE DATABASE db_instructores;
+CREATE DATABASE db_vehiculos;
+CREATE DATABASE db_asignaciones;
+CREATE DATABASE db_cobros;
+CREATE DATABASE db_reportes;
+CREATE DATABASE db_notificaciones;
